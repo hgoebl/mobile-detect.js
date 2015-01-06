6d21e4022d705c35fce92f71b948ef66a6bf1588
Dmitry Fink <dmitry@finik.net>
2015-01-05 18:40:54 -0800

    Update mobile-detect.js

--- mobile-detect.js
+++ mobile-detect.js
@@ -256,7 +256,7 @@ define(function () {
         "DesktopMode": "WPDesktop",
         "TV": "SonyDTV|HbbTV",
         "WebKit": "(webkit)[ \/]([\\w.]+)",
-        "Bot": "Googlebot|AdsBot-Google|YandexBot|bingbot|ia_archiver|AhrefsBot|Ezooms|GSLFbot|WBSearchBot|Twitterbot|TweetmemeBot|Twikle|PaperLiBot|Wotbox|UnwindFetchor|facebookexternalhit",
+        "Bot": "Googlebot|AdsBot-Google|Google Keyword Suggestion|YandexBot|bingbot|ia_archiver|AhrefsBot|Ezooms|GSLFbot|WBSearchBot|Twitterbot|TweetmemeBot|Twikle|PaperLiBot|Wotbox|UnwindFetchor|facebookexternalhit",
         "MobileBot": "Googlebot-Mobile|AdsBot-Google-Mobile|YahooSeeker\/M1A1-R2D2",
         "Console": "\\b(Nintendo|Nintendo WiiU|Nintendo 3DS|PLAYSTATION|Xbox)\\b",
         "Watch": "SM-V700"
